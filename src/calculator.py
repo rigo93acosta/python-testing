2d5ac48dbcf401758ac0786befe0280cbb2015d2
Luis Martinez <lcmartinezl05@gmail.com>
2024-08-26 19:02:33 -0500

    Clase 3: Introducción a la Biblioteca Unittest

--- src/calculator.py
+++ src/calculator.py
@@ -3,3 +3,11 @@ def sum(a, b):
 
 def subtract(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("La división por cero no está permitida")
+    return a / b
