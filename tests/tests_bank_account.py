4324b1f5fc52cd427ff1e84821633269c30a5de5
Luis Martinez <lcmartinezl05@gmail.com>
2024-08-26 19:57:41 -0500

    Clase 5: Uso de tearDown para Limpieza de Pruebas

--- tests/tests_bank_account.py
+++ tests/tests_bank_account.py
@@ -1,11 +1,19 @@
-import unittest
+import unittest, os
 
 from src.bank_account import BankAccount
 
 class BankAccountTests(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.account = BankAccount(balance=1000)
+        self.account = BankAccount(balance=1000, log_file="transaction_log.txt")
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, "r") as f:
+            return len(f.readlines())
 
     def test_deposit(self):
         new_balance = self.account.deposit(500)
@@ -14,6 +22,15 @@ class BankAccountTests(unittest.TestCase):
     def test_withdraw(self):
         new_balance = self.account.withdraw(200)
         assert new_balance == 800
-    
+
     def test_get_balance(self):
         assert self.account.get_balance() == 1000
+
+    def test_transaction_log(self):
+        self.account.deposit(500)
+        assert os.path.exists("transaction_log.txt")
+
+    def test_count_transactions(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self._count_lines(self.account.log_file) == 2
