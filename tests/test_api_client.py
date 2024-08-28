b43883b428b1183fa08a6b701efd6a88db2afd17
Luis Martinez <lcmartinezl05@gmail.com>
2024-08-28 18:01:29 -0500

    Clase 11: Uso de Side Effects en Mocking

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,4 +1,5 @@
-import unittest
+import unittest, requests
+import unittest.mock
 from src.api_client import get_location
 from unittest.mock import patch
 
@@ -19,3 +20,25 @@ class ApiClientTests(unittest.TestCase):
         self.assertEqual(result.get("city"), "MIAMI")
 
         mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8")
+
+    @patch("src.api_client.requests.get")
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Service Unavailable"),
+            unittest.mock.Mock(
+                status_code=200,
+                json=lambda: {
+                    "countryName": "USA",
+                    "regionName": "FLORIDA",
+                    "cityName": "MIAMI",
+                },
+            ),
+        ]
+
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")
+
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"), "USA")
+        self.assertEqual(result.get("region"), "FLORIDA")
+        self.assertEqual(result.get("city"), "MIAMI")
